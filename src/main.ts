51cc77e4bddddd21375e9298706d20cb190f1d21
voskanvl <voskanvl@gmail.com>
2022-09-19 18:58:03 +0300

    card-list

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,27 @@ import "@splidejs/splide/css";
 import toggleSidePanel from "./toggleSlidePanel";
 
 const splidesInstance = new MSplides();
-splidesInstance.add("#chairtypes", {
-    type: "loop",
-    autoplay: false,
-    perMove: 1,
-    direction: "rtl",
-    arrows: false,
-    focus: "center",
-    pagination: false,
-    autoWidth: true,
-});
+// splidesInstance.add("#chairtypes", {
+//     type: "loop",
+//     autoplay: false,
+//     perMove: 1,
+//     direction: "rtl",
+//     arrows: false,
+//     focus: "center",
+//     pagination: false,
+//     autoWidth: true,
+// });
 splidesInstance.add("#tiser", {
     type: "loop",
     arrows: false,
     pagination: false,
     perPage: 1,
+});
+splidesInstance.add("#card-list", {
+    type: "loop",
+    arrows: false,
+    pagination: false,
+    autoWidth: true,
 });
 
 const tiserControls = document.querySelector(".tiser__controls")?.children;
@@ -31,44 +37,14 @@ tiserControls?.length &&
         splidesInstance.instances["#tiser"].go(">");
     });
 
-const chairtypesButtons = document.querySelectorAll(".chairtypes__button");
-chairtypesButtons[0].addEventListener("click", () => {
-    splidesInstance.instances["#chairtypes"].go("<");
-});
-chairtypesButtons[1].addEventListener("click", () => {
-    splidesInstance.instances["#chairtypes"].go(">");
-});
-
-const blogButtons = document.querySelectorAll(".blog__button");
-const blogContents = document.querySelectorAll(".blog__content");
-
-const removeActive = () => {
-    blogContents.forEach(e => e.removeAttribute("active"));
-};
-const setActive = (index: number) => {
-    removeActive();
-    blogContents.forEach(e => {
-        if (e.getAttribute("data-id") === String(index))
-            e.setAttribute("active", "active");
-    });
-};
-
-blogButtons[0].addEventListener("click", () => {
-    splidesInstance.instances["#blog-slider"].go(">");
-    setActive(splidesInstance.instances["#blog-slider"].index);
-    console.log(splidesInstance.instances["#blog-slider"].index);
-});
-blogButtons[1].addEventListener("click", () => {
-    splidesInstance.instances["#blog-slider"].go("<");
-    removeActive();
-    setActive(splidesInstance.instances["#blog-slider"].index);
-    console.log(splidesInstance.instances["#blog-slider"].index);
-});
-
-splidesInstance.instances["#blog-slider"].on("moved", () =>
-    setActive(splidesInstance.instances["#blog-slider"].index),
-);
-
-setActive(0);
+const cardListControls = document.querySelectorAll(".card-list > .arrow");
+cardListControls.length === 2 &&
+    cardListControls[0].addEventListener("click", () =>
+        splidesInstance.instances["#card-list"].go("<"),
+    );
+cardListControls.length === 2 &&
+    cardListControls[1].addEventListener("click", () =>
+        splidesInstance.instances["#card-list"].go(">"),
+    );
 
 toggleSidePanel(".side-panel", "#trigger");
