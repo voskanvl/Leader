65a38ffbaf60e0b6e15e6e3ecc8524b2ee979bdb
voskanvl <voskanvl@gmail.com>
2022-09-21 10:46:36 +0300

    responsive fixes

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // import pugPlugin from "vite-plugin-pug";
-import vitePugPlugin from './vite-plugin-pug-edited';
-import { resolve, extname, posix } from 'path';
-import { readFileSync, readdirSync } from 'fs';
-import * as yaml from 'js-yaml';
-import colors from 'picocolors';
+import vitePugPlugin from "./vite-plugin-pug-edited";
+import { resolve, extname, posix } from "path";
+import { readFileSync, readdirSync } from "fs";
+import * as yaml from "js-yaml";
+import colors from "picocolors";
 
 const options = { pretty: true }; // FIXME: pug pretty is deprecated!
 const locals = {
-    name: 'ШВСМ',
+    name: "ШВСМ",
 };
 
-const STANDART = 'yaml';
+const STANDART = "yaml";
 
 function getShortName(file, root) {
-    return file.startsWith(root + '/') ? posix.relative(root, file) : file;
+    return file.startsWith(root + "/") ? posix.relative(root, file) : file;
 }
 
 const merge = standart => {
     console.log(`now merging ${STANDART} files`);
     const fn = { json: JSON.stringify, yaml: yaml.load }[standart];
-    const files = readdirSync(resolve(__dirname, 'src/data'));
-    const jsons = files.filter(file => extname(file) === '.' + STANDART);
+    const files = readdirSync(resolve(__dirname, "src/data"));
+    const jsons = files.filter(file => extname(file) === "." + STANDART);
     return jsons.reduce(
         (acc, file) => ({
             ...acc,
-            ...fn(readFileSync(resolve(__dirname, 'src/data', file))),
+            ...fn(readFileSync(resolve(__dirname, "src/data", file))),
         }),
         {},
     );
@@ -33,19 +33,19 @@ const merge = standart => {
 
 function CustomHmr() {
     return {
-        name: 'custom-hmr',
-        enforce: 'post',
+        name: "custom-hmr",
+        enforce: "post",
         // HMR
         handleHotUpdate({ file, server }) {
-            console.log('file>', file);
-            if (file.endsWith('.' + STANDART)) {
+            console.log("file>", file);
+            if (file.endsWith("." + STANDART)) {
                 server.config.logger.info(
-                    colors.green(STANDART + ' reload ') +
+                    colors.green(STANDART + " reload ") +
                         colors.dim(getShortName(file, server.config.root)),
                     { clear: true, timestamp: true },
                 );
                 server.ws.send({
-                    type: 'full-update',
+                    type: "full-update",
                 });
             }
         },
@@ -60,15 +60,16 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
+                main: resolve(__dirname, "index.html"),
                 // second: resolve(__dirname, "second.html"),
             },
         },
     },
     resolve: {
         alias: {
-            '@': resolve(__dirname, './src'),
-            '@assets': resolve(__dirname, './assets'),
+            "@": resolve(__dirname, "./src"),
+            "@assets": resolve(__dirname, "./assets"),
+            "@var": resolve(__dirname, "./src/sass/_variables.sass"),
         },
     },
 });
