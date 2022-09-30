cd67ff0784a219360ad7553caf5135c417dcdd76
voskanvl <voskanvl@gmail.com>
2022-09-30 10:32:21 +0300

    slider galary

--- src/galary.ts
+++ src/galary.ts
@@ -1,18 +1,21 @@
 export class Galary {
-    big: HTMLImageElement | null
-    thumbnail: HTMLImageElement[]
-    active: number
-    constructor(big: string, thumbnail: string,) {
-        this.big = document.querySelector(big)
-        this.thumbnail = Array.from(document.querySelectorAll(thumbnail))
-        this.active = 0
-        this.thumbnail.forEach(el => el.addEventListener("click", ({ target }) => {
-            // const {dataset} = target as HTMLImageElement
-            const big = (target as HTMLImageElement).getAttribute("big")
-            if (big) this.switchImage(big)
-        }))
+    big: HTMLImageElement | null;
+    thumbnail: HTMLImageElement[];
+    active: number;
+    constructor(big: string, thumbnail: string) {
+        this.big = document.querySelector(big);
+        this.thumbnail = Array.from(document.querySelectorAll(thumbnail));
+        console.log("🚀 ~ this.thumbnail", this.thumbnail);
+        this.active = 0;
+        this.thumbnail.forEach(el =>
+            el.addEventListener("click", ({ target }) => {
+                // const {dataset} = target as HTMLImageElement
+                const big = (target as HTMLImageElement).getAttribute("big");
+                if (big) this.switchImage(big);
+            }),
+        );
     }
     private switchImage(src: string) {
-        if (this.big && this.big.src) this.big.src = src
+        if (this.big && this.big.src) this.big.src = src;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,85 @@
-import { Counter } from "./counter"
-import "./sass/style.sass"
-import { MSplides } from "./initSlides"
-import "@splidejs/splide/css"
-import toggleSidePanel from "./toggleSlidePanel"
-import { Tabs } from "./tabs"
-import { Galary } from "./galary"
-import { CardStage } from "./cardStage"
+import { Counter } from "./counter";
+import "./sass/style.sass";
+import { MSplides } from "./initSlides";
+import "@splidejs/splide/css";
+import toggleSidePanel from "./toggleSlidePanel";
+import { Tabs } from "./tabs";
+import { Galary } from "./galary";
+import { CardStage } from "./cardStage";
 
-const splidesInstance = new MSplides()
-const tiserEl = document.querySelector("#tiser")
-const cardListEl = document.querySelector("#card-list")
+const splidesInstance = new MSplides();
+const tiserEl = document.querySelector("#tiser");
+const cardListEl = document.querySelector("#card-list");
+const galaryEl = document.querySelector("#galary");
 tiserEl &&
     splidesInstance.add("#tiser", {
         type: "loop",
         arrows: false,
         pagination: false,
         perPage: 1,
-    })
+    });
 cardListEl &&
     splidesInstance.add("#card-list", {
         type: "loop",
         arrows: false,
         pagination: false,
         autoWidth: true,
-    })
+    });
+galaryEl &&
+    splidesInstance.add("#galary", {
+        type: "loop",
+        arrows: false,
+        pagination: false,
+        perPage: 4,
+        perMove: 1,
+    });
 
-const tiserControls = document.querySelector(".tiser__controls")?.children
+const tiserControls = document.querySelector(".tiser__controls")?.children;
 tiserControls?.length &&
     tiserControls[0].addEventListener("click", () => {
-        splidesInstance.instances["#tiser"].go("<")
-    })
+        splidesInstance.instances["#tiser"].go("<");
+    });
 tiserControls?.length &&
     tiserControls[1].addEventListener("click", () => {
-        splidesInstance.instances["#tiser"].go(">")
-    })
+        splidesInstance.instances["#tiser"].go(">");
+    });
 
-const cardListControls = document.querySelectorAll(".card-list > .arrow")
+const cardListControls = document.querySelectorAll(".card-list > .arrow");
 cardListControls.length === 2 &&
     cardListControls[0].addEventListener("click", () =>
         splidesInstance.instances["#card-list"].go("<"),
-    )
+    );
 cardListControls.length === 2 &&
     cardListControls[1].addEventListener("click", () =>
         splidesInstance.instances["#card-list"].go(">"),
-    )
+    );
 
-new Tabs(".tabs__button", ".tabs__item")
-new Galary(".galary__big > img", ".galary__thumbnail")
+const galaryControls = document.querySelectorAll(".galary__slider > .arrow");
+galaryControls.length === 2 &&
+    galaryControls[0].addEventListener("click", () =>
+        splidesInstance.instances["#galary"].go("<"),
+    );
+galaryControls.length === 2 &&
+    galaryControls[1].addEventListener("click", () =>
+        splidesInstance.instances["#galary"].go(">"),
+    );
 
-new Counter(".counter")
+new Tabs(".tabs__button", ".tabs__item");
+new Galary(".galary__big > img", ".galary__slider > .splide img");
 
-toggleSidePanel(".side-panel", "#burger")
+new Counter(".counter");
 
-new CardStage([
-    document.querySelector(".shop-card__article[data-id='0'] > button.button "),
-    document.querySelector(".shop-card__article[data-id='1'] > button.button "),
-    document.querySelector(".shop-card__article[data-id='2']  button.button ")
-])
+toggleSidePanel(".side-panel", "#burger");
+
+if (document.querySelector(".shop-card"))
+    new CardStage([
+        document.querySelector(
+            ".shop-card__article[data-id='0'] > button.button ",
+        ),
+        document.querySelector(
+            ".shop-card__article[data-id='1'] > button.button ",
+        ),
+        document.querySelector(
+            ".shop-card__article[data-id='2']  button.button ",
+        ),
+    ]);
