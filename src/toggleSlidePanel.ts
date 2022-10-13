1d3b4e9daa8eb0de966a197d34305a96941c975e
voskanvl <voskanvl@gmail.com>
2022-10-13 10:57:11 +0300

    add X + fix side-panel

--- src/toggleSlidePanel.ts
+++ src/toggleSlidePanel.ts
@@ -1,15 +1,20 @@
-function toggleSidePanel(selector:string, triggerSelector: string){
-    const panel = document.querySelector(selector)
-    const trigger = document.querySelector(triggerSelector)
-    if(!panel || !trigger) return
-    trigger.addEventListener("click",(e)=>{
-        e.preventDefault()
-        e.stopPropagation()
-        panel.setAttribute('open','open')
-    })
-    document.addEventListener("click",({target})=>{
-        if((target as Element).closest(selector) !== panel) panel.removeAttribute("open")
-    })
+function toggleSidePanel(selector: string, triggerSelector: string) {
+    const panel = document.querySelector(selector);
+    const trigger = document.querySelector(triggerSelector);
+    if (!panel || !trigger) return;
+
+    const close = ({ target }: Event) => {
+        if ((target as Element).closest(selector) !== panel) panel.removeAttribute("open");
+    };
+
+    trigger.addEventListener("click", e => {
+        e.preventDefault();
+        e.stopPropagation();
+        panel.setAttribute("open", "open");
+    });
+    document.addEventListener("click", close);
+    const closeElement = document.querySelector(".side-panel__close");
+    closeElement && closeElement.addEventListener("click", () => panel.removeAttribute("open"));
 }
 
-export default toggleSidePanel
+export default toggleSidePanel;
